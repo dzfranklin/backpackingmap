6a4ad9610c4e608757fb1fb1adf7a98fbb8b6a5d
Daniel Franklin <daniel@danielzfranklin.org>
2020-09-25 22:09:17 +0100

    Remove starter app folder

--- app/app/src/main/java/org/danielzfranklin/hikingmap/ui/login/LoginFormState.kt
+++
@@ -1,8 +0,0 @@
-package org.danielzfranklin.hikingmap.ui.login
-
-/**
- * Data validation state of the login form.
- */
-data class LoginFormState(val usernameError: Int? = null,
-                          val passwordError: Int? = null,
-                          val isDataValid: Boolean = false)
