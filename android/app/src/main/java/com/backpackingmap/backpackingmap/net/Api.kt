728d4e75a4fde291ed60b8555d8ec21a7676e0c3
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-03 20:38:29 +0100

    Add Authorization parameter to api calls

--- android/app/src/main/java/com/backpackingmap/backpackingmap/net/Api.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/net/Api.kt
@@ -6,10 +6,9 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.POST
+import retrofit2.http.*
 
+private const val AUTHORIZATION = "Authorization"
 private const val BASE_URL = BuildConfig.API_BASE_URL
 
 private val moshi = Moshi.Builder()
@@ -31,10 +30,10 @@ interface ApiService {
             Response<AuthInfo, CreateSessionResponseError>
 
     @DELETE("session")
-    suspend fun deleteSession()
+    suspend fun deleteSession(@Header(AUTHORIZATION) auth: String)
 
     @POST("/session/renew")
-    suspend fun renewSession():
+    suspend fun renewSession(@Header(AUTHORIZATION) auth: String):
             Response<AuthInfo, RenewSessionResponseError>
 }
 
