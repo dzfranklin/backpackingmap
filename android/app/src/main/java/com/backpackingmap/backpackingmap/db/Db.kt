35c131d2307d397c281409d1a007662ddbca23c4
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-04 15:54:10 +0100

    Use cleaner syntax for singleton db

--- android/app/src/main/java/com/backpackingmap/backpackingmap/db/Db.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/db/Db.kt
@@ -4,39 +4,23 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import arrow.syntax.function.memoize
 import com.backpackingmap.backpackingmap.db.user.User
 import com.backpackingmap.backpackingmap.db.user.UserDao
-import kotlinx.coroutines.InternalCoroutinesApi
-import kotlinx.coroutines.internal.synchronized
 
-// From <https://codelabs.developers.google.com/codelabs/android-room-with-a-view-kotlin/index.html?index=..%2F..index#7>
 @Database(entities = [User::class], version = 1)
 abstract class Db : RoomDatabase() {
     abstract fun userDao(): UserDao
 
     companion object {
-        // Singleton prevents multiple instances of the db opening at the same time
-        @Volatile private var INSTANCE: Db? = null
+        val getDatabase = ::getDatabaseUnmemoized.memoize()
 
-        @OptIn(InternalCoroutinesApi::class)
-        fun getDatabase(context: Context): Db {
-            val tempInstance = INSTANCE
-
-            if (tempInstance != null) {
-                return tempInstance
-            }
-
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    Db::class.java,
-                    "backpackingmap_database"
-                ).build()
-
-                INSTANCE = instance
-
-                return instance
-            }
+        private fun getDatabaseUnmemoized(context: Context): Db {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                Db::class.java,
+                "backpackingmap_database"
+            ).build()
         }
     }
 }
