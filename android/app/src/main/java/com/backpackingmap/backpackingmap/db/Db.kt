50a409e6641f53557d7d6994b46d915f47796d47
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-04 17:05:55 +0100

    Write migration for previous change to user
    that dropped access_token

--- android/app/src/main/java/com/backpackingmap/backpackingmap/db/Db.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/db/Db.kt
@@ -4,11 +4,24 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import arrow.syntax.function.memoize
 import com.backpackingmap.backpackingmap.db.user.User
 import com.backpackingmap.backpackingmap.db.user.UserDao
 
-@Database(entities = [User::class], version = 1)
+val MIGRATIONS: Array<Migration> = arrayOf(
+    object : Migration(1, 2) {
+        override fun migrate(database: SupportSQLiteDatabase) {
+            database.execSQL("CREATE TABLE users_new ('__enforceIsSingleton' INTEGER NOT NULL, id INTEGER NOT NULL, renewal_token TEXT NOT NULL, PRIMARY KEY('__enforceIsSingleton'))")
+            database.execSQL("INSERT INTO users_new ('__enforceIsSingleton', id, renewal_token) SELECT '__enforceIsSingleton', id, renewal_token FROM users")
+            database.execSQL("DROP TABLE users")
+            database.execSQL("ALTER TABLE users_new RENAME TO users")
+        }
+    }
+)
+
+@Database(entities = [User::class], version = 2)
 abstract class Db : RoomDatabase() {
     abstract fun userDao(): UserDao
 
@@ -20,7 +33,9 @@ abstract class Db : RoomDatabase() {
                 context.applicationContext,
                 Db::class.java,
                 "backpackingmap_database"
-            ).build()
+            )
+                .addMigrations(*MIGRATIONS)
+                .build()
         }
     }
 }
