80909b043598548babc7f872d20ad344d9923096
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-04 17:05:08 +0100

    Prelim map activity that tries to display one tile

--- android/app/src/main/java/com/backpackingmap/backpackingmap/map_activity/MapActivity.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/map_activity/MapActivity.kt
@@ -2,16 +2,27 @@ package com.backpackingmap.backpackingmap.map_activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.backpackingmap.backpackingmap.R
+import com.backpackingmap.backpackingmap.databinding.ActivityMapBinding
 import com.backpackingmap.backpackingmap.enforceLoggedIn
 
 class MapActivity: AppCompatActivity() {
+    lateinit var binding: ActivityMapBinding
     lateinit var model: MapViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enforceLoggedIn(this)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_map)
         model = ViewModelProvider(this).get(MapViewModel::class.java)
+
+        model.tile.observe(this, {
+            it?.let {
+                binding.imageView.setImageBitmap(it)
+            }
+        })
     }
 }
--- android/app/src/main/java/com/backpackingmap/backpackingmap/map_activity/MapViewModel.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/map_activity/MapViewModel.kt
@@ -1,6 +1,22 @@
 package com.backpackingmap.backpackingmap.map_activity
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.graphics.Bitmap
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.backpackingmap.backpackingmap.net.tile.TileType
+import com.backpackingmap.backpackingmap.repo.Repo
+import kotlinx.coroutines.launch
 
-class MapViewModel(): ViewModel() {
+class MapViewModel(application: Application): AndroidViewModel(application) {
+    private val repo: Repo? = Repo.fromApplication(application)
+
+    val tile = MutableLiveData<Bitmap>()
+
+    init {
+        viewModelScope.launch {
+            tile.value = repo?.getTile(TileType.Explorer, 2025, 773)
+        }
+    }
 }
