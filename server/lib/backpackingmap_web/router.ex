a4353d2367f55ffb487ac030b0809b55290202c1
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-18 12:07:03 +0100

    Fix typo

--- server/lib/backpackingmap_web/router.ex
+++ server/lib/backpackingmap_web/router.ex
@@ -45,7 +45,7 @@ defmodule BackpackingmapWeb.Router do
   scope "/api/v1", BackpackingmapWeb.API.V1, as: :api_v1 do
     pipe_through [:api, :api_protected]
 
-    post "/tile/", TileController, :post
+    post "/tile", TileController, :post
   end
 
   scope "/api/v1", BackpackingmapWeb.API.V1, as: :v1_api do
