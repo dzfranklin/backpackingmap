0dbc1cbce22b31e48815b9ad47a62accd4d02308
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-20 14:28:30 +0100

    Fix accidentally committed experimentation

--- android/app/src/main/java/com/backpackingmap/backpackingmap/map/ScreenCoordinate.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/map/ScreenCoordinate.kt
@@ -13,25 +13,3 @@ data class ScreenCoordinate(val x: Float, val y: Float) {
             ScreenCoordinate(event.x, event.y)
     }
 }
-
-data class Foo(val a: Int, val b: Int, val c: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Foo
-
-        if (a != other.a) return false
-        if (b != other.b) return false
-        if (c != other.c) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = a
-        result = 31 * result + b
-        result = 31 * result + c
-        return result
-    }
-}
