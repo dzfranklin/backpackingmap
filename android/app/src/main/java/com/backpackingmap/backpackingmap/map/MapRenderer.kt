b90d6c0348c4a1e0cd9b6d0c30a4b916f09e5bbf
Daniel Franklin <daniel@danielzfranklin.org>
2020-10-26 16:47:17 +0000

    Remove unnecessary mutability

--- android/app/src/main/java/com/backpackingmap/backpackingmap/map/MapRenderer.kt
+++ android/app/src/main/java/com/backpackingmap/backpackingmap/map/MapRenderer.kt
@@ -4,13 +4,13 @@ import android.graphics.Canvas
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class, FlowPreview::class)
 class MapRenderer constructor(
     override val coroutineContext: CoroutineContext,
-    private val layers: MutableStateFlow<List<MapLayer>>,
+    private val layers: StateFlow<List<MapLayer>>,
 ) : CoroutineScope {
     fun renderTo(canvas: Canvas) {
         for (layer in layers.value) {
