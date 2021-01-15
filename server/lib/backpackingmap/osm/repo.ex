5c327f1a8713c65fdf2106548a03ee104c35550e
Daniel Franklin <daniel@danielzfranklin.org>
2021-01-15 16:17:10 +0000

    Format

--- server/lib/backpackingmap/osm/repo.ex
+++ server/lib/backpackingmap/osm/repo.ex
@@ -1,5 +1,6 @@
 defmodule Backpackingmap.Osm.Repo do
   use Ecto.Repo,
-    otp_app: :backpackingmap,
-    adapter: Ecto.Adapters.Postgres
+      otp_app: :backpackingmap,
+      adapter: Ecto.Adapters.Postgres
+
 end
