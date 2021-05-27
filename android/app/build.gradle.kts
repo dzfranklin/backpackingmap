2ac454e91b45a6433eb1b4f4cad291202f5425f7
Daniel Franklin <daniel@danielzfranklin.org>
2021-05-27 17:21:43 +0100

    Remove deprecated

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -41,7 +41,6 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
-        kotlinCompilerVersion = "1.4.32"
     }
 }
 
